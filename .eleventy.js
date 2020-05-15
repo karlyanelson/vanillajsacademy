bd352dbe3a71cd73ec1b492115b3df6b0888f824
Karly Nelson <karlyanelson@gmail.com>
2020-05-14 22:23:03 -0500

    add netlify toml

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const CleanCSS = require("clean-css");
 module.exports = function(eleventyConfig) {
   
     // eleventyConfig.setTemplateFormats([
-    //   "css" // css is not yet a recognized template extension in Eleventy
+    //   // "css" // css is not yet a recognized template extension in Eleventy
     // ]);
 
     // https://www.11ty.dev/docs/quicktips/inline-css/
