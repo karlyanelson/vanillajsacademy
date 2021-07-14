e725aa1c118916e197d2779688f262b70dc7909b
Nelson, Karly <karly.nelson@hey.com>
2021-07-14 10:54:43 -0500

    proj 20

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,35 @@
 // docs: https://www.11ty.io/docs/config/
 const CleanCSS = require("clean-css");
 
-module.exports = function(eleventyConfig) {
-    // eleventyConfig.setTemplateFormats([
-    //   "css" // css is not yet a recognized template extension in Eleventy
-    // ]);
+module.exports = function (eleventyConfig) {
+  // eleventyConfig.setTemplateFormats([
+  //   "css" // css is not yet a recognized template extension in Eleventy
+  // ]);
 
-    // Copy over assets/ folder
-    eleventyConfig.addPassthroughCopy({ "src/vanillajsacademy/assets/": "./vanillajsacademy/assets/" });
+  //// if you want a separate css file, instead of inlined
+  // eleventyConfig.addPassthroughCopy({ "src/css/": "./css/" });
+  // eleventyConfig.addWatchTarget("./src/css/");
 
-    // https://www.11ty.dev/docs/quicktips/inline-css/
-    eleventyConfig.addFilter("cssmin", function(code) { 
-      return new CleanCSS({}).minify(code).styles;
+  //// Copy over assets/ folder
+  eleventyConfig.addPassthroughCopy({
+    "src/vanillajsacademy/assets/": "./vanillajsacademy/assets/",
+  });
+
+  //// https://www.11ty.dev/docs/quicktips/inline-css/
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addCollection("orderedProjects", function (collection) {
+    return collection.getFilteredByTag("projects").sort((a, b) => {
+      return a.data.order - b.data.order;
     });
-   
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
-    };
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
   };
+};
